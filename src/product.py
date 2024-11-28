58e7fdb1f05c5986801526f75e636ae3ffc7bf4c
igort <igortsevelev3@gmail.com>
2024-11-28 22:38:33 +0500

    Third_com

--- src/product.py
+++ src/product.py
@@ -2,10 +2,11 @@ class Product:
     """Класс товаров"""
     name: str
     description: str
-    price: str
-    quantity: str
+    price: float
+    quantity: int
 
     def __init__(self, name, description, price, quantity):
+        """Инициализация товаров"""
         self.name = name
         self.price = price
         self.description = description
---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture()
+def category_smartphone():
+    return Category("Смартфоны",
+                    "Смартфоны, как средство не только " "коммуникации, но и получения дополнительных "
+                    "функций для удобства жизни",
+                    ["Samsung Galaxy S23 Ultra", "Iphone 15", "Xiaomi Redmi Note 11"])
+
+
+@pytest.fixture()
+def product_samsung():
+    return Product("Samsung Galaxy S23 Ultra", "256GB, "
+                                               "Серый цвет, 200MP камера", 180000.0, 5)
